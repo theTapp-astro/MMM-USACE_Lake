951fa4990aeff0530d5c145688c2093938df107f
theTapp-astro <lisa.a.vicars@gmail.com>
2026-08-04 15:47:47 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+{
+    module: "MMM-USACELake",
+    position: "top_right",
+
+    config: {
+
+        office: "LRH",
+
+        location: "alumcr",
+
+        updateInterval: 300000
+
+    }
+
+}
