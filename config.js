754abb6742ee4104503cbb95a45ffd1e0332ae98
theTapp-astro <lisa.a.vicars@gmail.com>
2026-08-05 14:21:59 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 {
     module: "MMM-USACE_Lake",
     position: "top_right",
+    title: "Your Title Here",
     config: {
         office: "LRH",
-        location: "AlumCr",
+        location: "",
         refreshInterval: 300000, //5 minutes
         showTemperature: true,
         showStorage: true,
